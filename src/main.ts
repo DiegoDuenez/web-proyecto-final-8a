a1bdbba3fc01af2012231254bdda699b5d91a2e7
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-04-28 14:59:40 -0500

    del: console logs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+  
   enableProdMode();
+  window.console.log = () => {}
+
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
